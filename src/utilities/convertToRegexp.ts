e8a1c3be72582a24959cc10b95afc3b0bb2c0237
mainIdal <633mainidal@gmail.com>
2022-08-18 17:27:12 +0300

    Fixed bug in RegExp generator

--- src/utilities/convertToRegexp.ts
+++ src/utilities/convertToRegexp.ts
@@ -1,15 +1,16 @@
 export function convertToRegex(expression : string) : String {
-    const queries : String [] | null= expression.replaceAll(' ','').split(',');
-    
+    const queries : String [] | null= expression.split(',');
     let regex = [];
     try {
         let index = 0;
         for(let query of queries){
-            const command = `s2r ${query}`;
+            
+            query = query.replaceAll(`"`,`"""`);
+            const command = `s2r "${query}`;
 
             const {execSync} = require("child_process");
             regex[index] = execSync(command).toString().trim().replaceAll("class=","class\\s*=\\s*").replaceAll("id=","id\\s*=\\s*");
-            
+
             if (regex[index] === "" || regex[index].startsWith("(?<")){
                 throw new Error("Look behind assertion is detected");
             }
@@ -22,7 +23,9 @@ export function convertToRegex(expression : string) : String {
     }
     
     let finalRegex = regex.length ? regex.join('|') : "Invalid Css Selector Command";
-    finalRegex = finalRegex.replace(`${queries[0].split('.')[0]}`,`(?<!\\w)${queries[0].split('.')[0]}(?!\\w)`);
+    if(queries[0][0] !== '.'){
+        finalRegex = finalRegex.replace(`${queries[0].split('.')[0]}`,`(?<!\\w)${queries[0].split('.')[0]}(?!\\w)`);
+    }
 
     return finalRegex; 
 }

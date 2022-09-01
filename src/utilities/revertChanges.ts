c8ff2ef2b2af1e50f35ceb0be8406abd429e9d1b
mainIdal <633mainidal@gmail.com>
2022-09-01 16:23:39 +0300

    Added Informing the user about the progress of the rollback process

--- src/utilities/revertChanges.ts
+++ src/utilities/revertChanges.ts
@@ -1,14 +1,27 @@
 import * as vscode from 'vscode';
 
-export async function revertChanges(fileList: vscode.Uri[], rawContents: Uint8Array[], searchText: string, choice: string){
-    if (fileList.length > 0){
+export async function revertChanges(fileList: vscode.Uri[], rawContents: Uint8Array[], searchText: string, choice: string) {
+    if (fileList.length > 0) {
         try {
-            for(let index=0; index < fileList.length; index++){
-                await vscode.workspace.fs.writeFile(fileList[index], rawContents[index]);
-            }
-            vscode.window.showInformationMessage(`Rollback process of "${choice.toLowerCase()}" successful`);
-            rawContents.length = 0; fileList.length = 0;
-            vscode.commands.executeCommand("tag-manager.searchInFiles", searchText);
+            await vscode.window.withProgress({
+                location: vscode.ProgressLocation.Notification,
+                title: `Rollback process of "${choice.toLowerCase()}" is under the progress`,
+                cancellable: false
+            }, async (progress) => {
+                const numOfFiles = fileList.length;
+                let inc = numOfFiles > 0 ? Math.floor(100 / numOfFiles) : 100;
+                for (let index = 0; index < fileList.length; index++) {
+                    await vscode.workspace.fs.writeFile(fileList[index], rawContents[index]);
+                    progress.report({ increment: inc, message: `${inc * (index + 1)}% completed` });
+                }
+            });
+
+            setTimeout(() => {
+                rawContents.length = 0; fileList.length = 0;
+                vscode.window.showInformationMessage(`Rollback process of "${choice.toLowerCase()}" successful`);
+                vscode.commands.executeCommand("tag-manager.searchInFiles", searchText);
+            }, 1000);
+            
         } catch (error) {
             console.log(error);
             vscode.window.showErrorMessage(`Error occured during rollback process of "${choice.toLowerCase()}"`);

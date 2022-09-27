b0e607f2b5ed9c117e78d19b383eefcf92b2007f
mainIdal <633mainidal@gmail.com>
2022-09-27 21:52:14 +0300

    Added warning message to inform the user if no changes are required

--- src/utilities/notifyUser.ts
+++ src/utilities/notifyUser.ts
@@ -2,12 +2,10 @@ import * as vscode from 'vscode';
 
 export function notifyUser(processResult: string, warningMessage: string, searchText: string, replaceText: string, choice: string){
     if (processResult === ""){
-        vscode.window.showWarningMessage(warningMessage);
+        vscode.window.showWarningMessage(warningMessage !== "" ? warningMessage : "No modifications required as the desired changes are already there");
     } else if (processResult === "Success"){
         vscode.commands.executeCommand("search.action.refreshSearchResults").then(()=>{
-            if (replaceText === ""){
-                replaceText = searchText;
-            }
+            replaceText = replaceText !== "" ? replaceText : searchText;
             vscode.window.showInformationMessage(`${choice} process for "${replaceText}" successful`);
         });
     }else {

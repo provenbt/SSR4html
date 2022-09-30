3ae7c61e59c338c424d500f6412758383dd15d3d
mainIdal <633mainidal@gmail.com>
2022-09-30 15:18:55 +0300

    Removed unused line

--- src/utilities/replaceInFiles.ts
+++ src/utilities/replaceInFiles.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { replaceInFile } from './replaceInFile';
 
 export async function replaceInFiles(fileList: vscode.Uri[], rawContents: Uint8Array[], choice: string, searchText: string, replaceText: string) {
-    let processResults : string[] = [];
+    let processResults: string[] = [];
     let warningMessage: string = "";
 
     const files = await vscode.workspace.findFiles('**/*.html', '**/node_modules/**');
@@ -17,19 +17,18 @@ export async function replaceInFiles(fileList: vscode.Uri[], rawContents: Uint8A
             title: `${choice} process is under the progress`,
             cancellable: false
         }, async (progress) => {
-    
-            //If workspace is empty, it would give number/0 error
-            let inc = numOfFiles > 0 ? Math.round(100 / numOfFiles) : 100;
+
+            let inc = Math.round(100 / numOfFiles);
             let progressCounter = 0;
-    
+
             for (let file of files) {
                 const rawContent = await vscode.workspace.fs.readFile(file);
                 const htmlText = new TextDecoder().decode(rawContent);
-    
+
                 const result = await replaceInFile(htmlText, choice, searchText, replaceText, file, fileList, rawContents);
                 processResults.push(result.processResult);
                 warningMessage = result.warningMessage;
-                
+
                 progressCounter++;
                 let progressPercentage = inc * progressCounter;
                 progress.report({ increment: inc, message: `${progressPercentage < 100 ? progressPercentage : 100}% completed` });

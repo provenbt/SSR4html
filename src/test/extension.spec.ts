8ee22758b735627c0674ca655beef57818bd6b90
mainIdal <633mainidal@gmail.com>
2022-12-09 16:15:52 +0300

    Added test for the feature Exclude Files

--- src/test/extension.spec.ts
+++ src/test/extension.spec.ts
@@ -115,6 +115,33 @@ test.describe('Extension Tests', () => {
       await expect.soft(page.getByRole('dialog').locator('div').first()).toContainText(strings.nothingFoundToModifyInFileMessage);
     });
 
+    test('Exclude files', async () => {
+      // Create a search query, exclude files and 
+      await webview.getByRole('textbox', { name: strings.searchTextAreaTitle }).click();
+      await webview.getByRole('textbox', { name: strings.searchTextAreaTitle }).fill('div');
+      await webview.locator('#searchBox').dispatchEvent('keyup');
+
+      // Exclude files
+      await webview.locator('#searchInAll').check();
+      await webview.getByRole('textbox', { name: 'files to exclude' }).click();
+      await webview.getByRole('textbox', { name: 'files to exclude' }).fill('sample1*,   **/sample2.html');
+
+      // Trigger the search process
+      await webview.locator('#searchBtn').getByRole('button').click();
+
+      // Wait for the search results
+      await page.waitForSelector('div.messages.text-search-provider-messages > div.message');
+      // Verify that there are search results found
+      await expect.soft(page.locator('div.messages.text-search-provider-messages > div.message')).toHaveText(/^\d+ result/);
+
+      // Verfiy that the files are excluded
+      await page.getByRole('button', { name: '' }).click();;
+      expect.soft((await page.getByRole('textbox', { name: 'files to exclude' }).inputValue()).includes('**/sample1*,**/sample2.html')).toBeTruthy();
+
+      // Clear the search results
+      await webview.locator('#cancelBtn').getByRole('button').click();
+    });
+
     validSearchQueries.forEach(data => {
       test(data.title, async () => {
         // Create a search query and trigger the search process
@@ -129,7 +156,7 @@ test.describe('Extension Tests', () => {
         // Verify that there are search results found
         await expect.soft(page.locator('div.messages.text-search-provider-messages > div.message')).toHaveText(/^\d+ result/);
 
-        // Go back to the search part
+        // Clear the search results
         await webview.locator('#cancelBtn').getByRole('button').click();
       });
     });
@@ -214,75 +241,75 @@ test.describe('Extension Tests', () => {
       // Since the tests for the valid replacement operations may try to overwrite the same file(s) at the same time in case of parallelism,
       // these tests were serially connected
       test.describe.serial(`Replace ${value.title}`, () => {
-  
+
         test.beforeAll(async ({ browser }) => {
           const context = await browser.newContext();
           page = await context.newPage();
-  
+
           webview = await prepareTestEnvironment(page, value.folderName);
-  
+
           // Open an HTML document with at least one div element
           await page.getByRole('treeitem', { name: 'sample1.html' }).locator('a').click();
-  
+
           // Create a search query to find div elements
           await webview.getByRole('textbox', { name: strings.searchTextAreaTitle }).click();
           await webview.getByRole('textbox', { name: strings.searchTextAreaTitle }).fill('div');
           await webview.locator('#searchBox').dispatchEvent('keyup');
-  
+
           // Test each replacement operation in the workspace and in the current file
           value.inWorkspace ? await webview.locator('#searchInAll').check() : await webview.locator('#searchInAll').uncheck();
         });
-  
+
         test.afterAll(async () => {
           await page.close();
         });
-  
+
         for (let data of validReplacementOperations) {
           test(`${data.title} ${value.title}`, async () => {
-  
+
             await test.step(`${data.title}(Apply the Replacement)`, async () => {
               // Clear all notifications that block screen
               await page.getByRole('button', { name: 'Notifications' }).click();
               await page.getByRole('button', { name: 'Clear All Notifications' }).click({ force: true });
-  
+
               // Start search query
               await webview.locator('#searchBtn').getByRole('button').click();
-  
+
               // Wait for the search results
               await page.waitForSelector('div.messages.text-search-provider-messages > div.message');
               // Verify that there are search results found
               await expect.soft(page.locator('div.messages.text-search-provider-messages > div.message')).toHaveText(/^\d+ result/);
-  
+
               // Open the list of replacement operations
               await webview.locator('#selection').click();
               // Select a replacement process
               await webview.getByRole('option', { name: data.replacementProcess }).click();
-  
+
               // If a replacement text is necessary to perform the replacement process, enter a valid replacement text 
               if (data.replacementText !== null) {
                 await webview.getByRole('textbox', { name: strings.replaceTextAreaDefaultTitle }).click();
                 await webview.getByRole('textbox', { name: strings.replaceTextAreaDefaultTitle }).fill(data.replacementText);
                 await webview.locator('#replacementBox').dispatchEvent('keyup');
               }
-  
+
               // trigger the replacement process
               await webview.locator('#replaceBtn').getByRole('button').click();
-  
+
               // Verify that the replacement process is successfull
               await expect.soft(page.getByRole('dialog').locator('div').first()).toHaveText(`${strings.replacementProcessName} ${strings.successfulProcessMessage}`);
             });
-  
+
             await test.step(`${data.title}(Rollback the Replacement)`, async () => {
               // Clear all notifications that block screen
               await page.getByRole('button', { name: 'Notifications' }).click();
               await page.getByRole('button', { name: 'Clear All Notifications' }).click({ force: true });
-  
+
               // Revert changes after each replacement process (to preserve the structure of test files for other replacement tests)
               await webview.locator('#revertBtn').getByRole('button').click();
               // Verify that the rollback process is successfull
               await expect.soft(page.getByRole('dialog').locator('div').first()).toHaveText(`${strings.revertProcessName} ${strings.successfulProcessMessage}`);
             });
-  
+
             // Go back to the search part for a new replacement process
             await webview.locator('#cancelBtn').getByRole('button').click();
           });
